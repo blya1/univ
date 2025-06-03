fd04c97fb21d6fc3b9a129f5cece644cb3f4fa37
blya1 <fura01718@gmail.com>
2025-06-04 02:29:22 +0300

    Update helper.js

--- client/helper.js
+++ client/helper.js
@@ -1,5 +1,5 @@
 (async () => {
-    let socket = new WebSocket('wss://<render-url>');
+    let socket = new WebSocket('wss://univ-8ebo.onrender.com');
     let isScriptEnabled = false;
     let isScriptInitialized = false;
     let lastClick = null;
@@ -226,7 +226,7 @@
     socket.onclose = () => {
         console.log('helper.js: WebSocket closed, attempting reconnect in 5s');
         setTimeout(() => {
-            socket = new WebSocket('ws://localhost:8080');
+            socket = new WebSocket('ws://univ-8ebo.onrender.com');
             socket.onopen = () => {
                 console.log('helper.js: WebSocket reconnected');
                 socket.send(JSON.stringify({ role: 'helper' }));
